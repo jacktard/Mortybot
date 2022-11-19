cae4df21d98cc04a8e97b9fa10b1ed8a2af521d0
J K <118572073+jacktard@users.noreply.github.com>
2022-11-19 03:45:57 +0000

    changed custom answers into replies

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ client.on('messageCreate', msg => {
     else {
       
       if (String(msg.author.username) == "Doja" && String(msg.content).includes("bald")) {
-        msg.channel.send("he’s bald, he’s BALD");
+        msg.reply("he’s bald, he’s BALD");
       }
       else if (String(msg.author.username) == "Doja" && (Math.random() <= 0.2)) {
         msg.reply("U beat me whilst fully wbuffed in the retard raid, here's ur reward https://cdn.discordapp.com/attachments/924354712118100018/1008428756223930509/20220803_003826.jpg");
@@ -69,26 +69,26 @@ client.on('messageCreate', msg => {
       }
       else if (String(msg.author.username) == "fatnstrong") {
         if (performance.now() - lastSentMessage > 10000){
-          msg.channel.send("Go back to doing yoga, u fucking fruit");
+          msg.reply("Go back to doing yoga, u fucking fruit");
         }
       }
       else if (String(msg.author.username) == "flynn" && (Math.random() <= 0.2)) {
         msg.reply("show bald head");
       }
       else if (String(msg.author.username) == "flynn" && (Math.random() <= 0.2)) {
-        msg.channel.send("ur actively griefing with ur bald head, go grow sum hair u hairless rat");
+        msg.reply("ur actively griefing with ur bald head, go grow sum hair u hairless rat");
       }
       else if (String(msg.author.username) == "TunasubGG" && (Math.random() <= 0.2)) {
-        msg.channel.send("Gg fish, ya'll beat a child in a 20 year old body");
+        msg.reply("Gg fish, ya'll beat a child in a 20 year old body");
       }
       else if (String(msg.author.username) == "TunasubGG" && (Math.random() <= 0.2)) {
-        msg.channel.send("I've lost respect for you fish after reading that.");
+        msg.reply("I've lost respect for you fish after reading that.");
       }
       else if (String(msg.author.username) == "Homeostasis" && (Math.random() <= 0.2)) {
-        msg.channel.send("You look like u eat wheaties everyday for breakfast, muscles ain't gonna save you from me running yo ass over🚬🚬🚬🚬");
+        msg.reply("You look like u eat wheaties everyday for breakfast, muscles ain't gonna save you from me running yo ass over🚬🚬🚬🚬");
       }
       else if (String(msg.author.username) == "Homeostasis" && (Math.random() <= 0.2)) {
-        msg.channel.send("Bend over big boy, I like em thicccc");
+        msg.reply("Bend over big boy, I like em thicccc");
       }
       else {
         msg.channel.send(mortyAnswer[Math.floor(Math.random() * mortyAnswer.length)]);
